0c45d8bdfd085e9da27c48d2690cdea569ea8674
Jesús Morales <capueagles@gmail.com>
2021-10-24 19:08:10 -0500

    Agregando comentarios a la clase MultiTable

--- Lab04/Lab04/src/MultiTable.java
+++ Lab04/Lab04/src/MultiTable.java
@@ -16,14 +16,21 @@ public class MultiTable {
 
         PolynomialProduct p = new PolynomialProduct();        
         MultiTable m = new MultiTable();
-        int n = 3; //Valor de n
-        int base = 2;
-        int cuadrado = (int)Math.pow( (double)base , (double)n );
 
+        /*-------THE VALUES MODIFY DEPEND OF THE MULTIPLICATION TABLE THAT WE WANT--------------*/
+        
+        int n = 6; //Valor de n
+        String mx = "1000011"; //Valor m(x)
+
+        /*--------------------------------------------------------------------------------------*/
+        
+        int base = 2;//Base
+        int cuadrado = (int)Math.pow( (double)base , (double)n );
+        
         //We create a string array
         String valores[] = new String[cuadrado-1];
 
-        //We input all the numbers that are element of 2^6
+        //We save all the numbers that are element of 2^n in the array valores
         for(int i = 1; i<cuadrado; i++)
         {
             String c = m.cadenadeBits(i, n);
@@ -33,12 +40,13 @@ public class MultiTable {
         //Now we do the multplication table
         try
         {
+            //We create the file that we save all the information
             BufferedWriter ficheroSalida = new BufferedWriter(new FileWriter(new File("multiplicationTable.txt")));
-            ficheroSalida.write("Multiplication table of 2^"+n+"\n\n*\t");
+            ficheroSalida.write("Multiplication table of 2^"+n+"\n\n*\t");//Write the title of the multiplication table
 
             for(int i = 0; i<cuadrado-1; i++)
             {
-                ficheroSalida.write(""+(i+1)+"\t");
+                ficheroSalida.write(""+(i+1)+"\t");//We write the line from 1 to 2^n in he filetext
             }
 
             ficheroSalida.write("\n");
@@ -48,7 +56,8 @@ public class MultiTable {
                 ficheroSalida.write(""+(i+1)+"\t");
                 for(int j = 0; j<cuadrado-1; j++)
                 {
-                    int res = p.getModularPolynomialProduct(3, valores[i], valores[j], "1011");
+                    //We call the function that we create before with the values of the array valores
+                    int res = p.getModularPolynomialProduct(n, valores[i], valores[j], mx);
                     ficheroSalida.write(""+res+"\t");
                 }
                 ficheroSalida.write("\n");
